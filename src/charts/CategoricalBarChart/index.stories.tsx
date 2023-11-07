7ef7d5a4da770d9a416340ce26f65caf478e1b56
maneetgoyal <mgoyal@manufacanalytics.com>
2023-11-07 14:11:38 +0530

    feat: Add CatgoricalBarChart

---
+++ src/charts/CategoricalBarChart/index.stories.tsx
@@ -0,0 +1,9 @@
+import { CategoricalBarChart } from ".";
+import type { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "CategoricalBarChart",
+  component: CategoricalBarChart,
+} as Meta<typeof CategoricalBarChart>;
+
+export const Template: StoryObj<typeof CategoricalBarChart> = {};
---
+++ src/charts/CategoricalBarChart/index.tsx
@@ -0,0 +1,159 @@
+import { BarChart as BarGraph } from "echarts/charts";
+import {
+  GridComponent,
+  TitleComponent,
+  DatasetComponent,
+  DataZoomComponent,
+  TooltipComponent,
+  LegendComponent,
+} from "echarts/components";
+import { getInstanceByDom, init, use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { useRef, useEffect } from "react";
+import type { CompleteStatistics } from "../../sample-data/state";
+import type { BarSeriesOption } from "echarts/charts";
+import type {
+  TitleComponentOption,
+  GridComponentOption,
+  DatasetComponentOption,
+  DataZoomComponentOption,
+  TooltipComponentOption,
+} from "echarts/components";
+import type { ComposeOption, ECharts } from "echarts/core";
+import type { CSSProperties } from "react";
+
+export type CategoricalBarChartOptions = ComposeOption<
+  | TitleComponentOption
+  | GridComponentOption
+  | BarSeriesOption
+  | DatasetComponentOption
+  | DataZoomComponentOption
+  | TooltipComponentOption
+>;
+
+use([
+  GridComponent,
+  TitleComponent,
+  BarGraph,
+  DatasetComponent,
+  DataZoomComponent,
+  TooltipComponent,
+  LegendComponent,
+  CanvasRenderer, // If you only need to use the canvas rendering mode, the bundle will not include the SVGRenderer module, which is not needed.
+]);
+
+export interface LineChartProps {
+  theme?: "light" | "dark";
+  data: CompleteStatistics[];
+  xAxis: string;
+  yAxis: string;
+  xAxisTitle: string;
+  yAxisTitle: string;
+  isLoading?: boolean;
+  title?: string;
+  style?: CSSProperties;
+}
+
+export function CategoricalBarChart({
+  theme,
+  data,
+  xAxis,
+  yAxis,
+  xAxisTitle,
+  yAxisTitle,
+  isLoading,
+  title,
+  style,
+}: LineChartProps): JSX.Element {
+  const chartRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let chart: ECharts | undefined;
+    let observer: ResizeObserver | undefined;
+
+    if (chartRef.current !== null) {
+      chart = init(chartRef.current, theme);
+      observer = new ResizeObserver(() => {
+        chart?.resize();
+      });
+      observer.observe(chartRef.current);
+    }
+
+    return () => {
+      chart?.dispose();
+      observer?.disconnect();
+    };
+  }, [theme]); // Whenever theme changes we need to dispose the chart to render a fresh one with appropriate styling
+
+  useEffect(() => {
+    if (chartRef.current !== null) {
+      const chart = getInstanceByDom(chartRef.current);
+      const option: CategoricalBarChartOptions = {
+        title: {
+          text: title,
+          left: "center",
+        },
+        grid: { bottom: "20%", top: "15%", right: "15%", left: "15%" },
+        xAxis: {
+          type: "category",
+          name: xAxisTitle ?? xAxis,
+          position: "bottom",
+          nameLocation: "middle",
+          nameGap: 30,
+          axisLine: { show: true },
+        },
+        yAxis: {
+          type: "value",
+          name: yAxisTitle ?? yAxis,
+          position: "left",
+          nameLocation: "middle",
+          nameGap: 50,
+          axisLine: { show: true },
+          scale: true,
+        },
+        dataset: {
+          dimensions: [xAxis, yAxis],
+          source: data,
+        },
+        series: [
+          {
+            type: "bar",
+            encode: {
+              x: xAxis,
+              y: yAxis,
+            },
+          },
+        ],
+        legend: {
+          align: "auto",
+          bottom: "1%",
+          type: "scroll",
+          orient: "horizontal",
+        },
+        dataZoom: [
+          {
+            type: "inside",
+            filterMode: "none", // Ref: https://stackoverflow.com/questions/53187396/echarts-line-dissapear-when-zooming-in
+            xAxisIndex: 0,
+          },
+        ],
+      };
+      chart?.setOption(option, true);
+    }
+  }, [data, theme, xAxis, xAxisTitle, yAxis, yAxisTitle, title]);
+
+  useEffect(() => {
+    if (chartRef.current !== null) {
+      const chart = getInstanceByDom(chartRef.current);
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      isLoading === true ? chart?.showLoading() : chart?.hideLoading();
+    }
+  }, [theme, isLoading]);
+
+  return (
+    <div
+      ref={chartRef}
+      style={{ height: "100%", width: "100%", borderWidth: "1px", borderStyle: "solid", ...style }}
+    />
+  );
+}
