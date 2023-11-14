47635da1117c89be61a26aa86f1d05b0faa7b9e4
maneetgoyal <mgoyal@manufacanalytics.com>
2023-11-14 08:24:28 +0530

    refactor: minor

--- src/sample-data/interfaces.ts
+++ src/sample-data/interfaces.ts
@@ -1,13 +1,4 @@
-// "uid": 49,
-// "runid": "at6104f3",
-// "timestamp": 1699782321,
-// "average_losses": [8.831217924753824, 8.843376654165763],
-// "average_loss": 8.837297289459794,
-// "win_rate": 1.0,
-// "win_total": 51
-
-// eslint-disable-next-line import/no-unused-modules
-export interface UIDDetails {
+interface UIDDetails {
   uid: number;
   runid: string;
   timestamp: number;
