0a878fd9e361747ecd355cd3d00907866d50a903
maneetgoyal <mgoyal@manufacanalytics.com>
2023-11-22 15:36:53 +0530

    Update script

--- amplify/backend/function/pretrainingfrontendf29a4f15/src/utils/script.py
+++ amplify/backend/function/pretrainingfrontendf29a4f15/src/utils/script.py
@@ -58,46 +58,47 @@ def merge_data(data):
             merged_data[f"validator-{key}"] = concatenated_list
 
 
-for run in runs:
-    # Parse the created_at time
-    try:
-        created_at = datetime.datetime.strptime(run.created_at, "%Y-%m-%dT%H:%M:%S")
-    except ValueError:
-        # Handle possible different time formats
-        created_at = datetime.datetime.strptime(run.created_at, "%Y-%m-%dT%H:%M:%S.%fZ")
+def fetch_wandb_data():
+  for run in runs:
+      # Parse the created_at time
+      try:
+          created_at = datetime.datetime.strptime(run.created_at, "%Y-%m-%dT%H:%M:%S")
+      except ValueError:
+          # Handle possible different time formats
+          created_at = datetime.datetime.strptime(run.created_at, "%Y-%m-%dT%H:%M:%S.%fZ")
 
-    # Calculate the time difference in days
-    time_diff = now - created_at
+      # Calculate the time difference in days
+      time_diff = now - created_at
 
-    # Check if the run was started less than 3 days ago and "validator" is in the run name
-    if time_diff.days < 3 and "validator" in run.name:
-        print(f"Processing run: {run.name}")
+      # Check if the run was started less than 3 days ago and "validator" is in the run name
+      if time_diff.days < 3 and "validator" in run.name:
+          print(f"Processing run: {run.name}")
 
-        # Retrieve the run history
-        run_data = run.history()
+          # Retrieve the run history
+          run_data = run.history()
 
-        # Extract the 'original_format_json' key
-        if 'original_format_json' in run_data.columns:
-            original_format_json_data = run_data['original_format_json']
+          # Extract the 'original_format_json' key
+          if 'original_format_json' in run_data.columns:
+              original_format_json_data = run_data['original_format_json']
 
-            # Convert the Pandas Series to a list or dict
-            if (isinstance(original_format_json_data, pd.Series)):
-                converted_data = []
-                target_list = original_format_json_data.to_list()
-                for ele in target_list:
-                    if isinstance(ele, str):
-                        converted_data.append(json.loads(ele))
-                    else:    
-                        converted_data.append(ele)
-            else:
-                converted_data = original_format_json_data
+              # Convert the Pandas Series to a list or dict
+              if (isinstance(original_format_json_data, pd.Series)):
+                  converted_data = []
+                  target_list = original_format_json_data.to_list()
+                  for ele in target_list:
+                      if isinstance(ele, str):
+                          converted_data.append(json.loads(ele))
+                      else:    
+                          converted_data.append(ele)
+              else:
+                  converted_data = original_format_json_data
 
-            # Replace NaN value and infinity values with null
-            converted_data = replace_inf_nan(converted_data)
-            all_run_data[run.name] = converted_data
+              # Replace NaN value and infinity values with null
+              converted_data = replace_inf_nan(converted_data)
+              all_run_data[run.name] = converted_data
 
-combined_data=merge_data(all_run_data)
-# Save the extracted data to a JSON file
-output_path = os.path.join(os.path.dirname(__file__), "multi.json")
-with open(output_path, 'w') as f:
-    json.dump(combined_data, f, indent=2)
+  combined_data=merge_data(all_run_data)
+  # Save the extracted data to a JSON file
+  output_path = os.path.join(os.path.dirname(__file__), "multi.json")
+  with open(output_path, 'w') as f:
+      json.dump(combined_data, f, indent=2)
