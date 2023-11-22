6db5054bf42b2f709c585232933be9b599e735ad
maneetgoyal <mgoyal@manufacanalytics.com>
2023-11-22 16:32:49 +0530

    Post data to s3

--- amplify/backend/function/pretrainingfrontendf29a4f15/src/index.py
+++ amplify/backend/function/pretrainingfrontendf29a4f15/src/index.py
@@ -1,8 +1,18 @@
 import json
 from utils.script import fetch_wandb_data
+import boto3
+import os
 
+s3_client = boto3.client('s3')
 def handler(event, context):
   fetch_wandb_data()
+  
+  multi_json_file_path='/utils/multi.json'
+  s3_client.upload_file(
+    Filename=multi_json_file_path,
+    Bucket=os.environ.get("STORAGE_S32500FDF3_BUCKETNAME"),
+    Key='s3_folder/multi.json'
+)
   return {
       'statusCode': 200,
       'headers': {
