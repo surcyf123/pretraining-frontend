d683ac5fc405785c41d2c700b0de919a53f5025e
maneetgoyal <mgoyal@manufacanalytics.com>
2023-11-23 14:02:34 +0530

    Fix ssl error

--- amplify/backend/function/wandb/src/utils/script.py
+++ amplify/backend/function/wandb/src/utils/script.py
@@ -6,6 +6,7 @@ api = wandb.Api()
 # Define the project and entity
 project_name = "pretraining-subnet"
 entity_name = "opentensor-dev"
+
 def test_wandb_initialization():
     # Retrieve all runs from the specified project
     runs = api.runs(f"{entity_name}/{project_name}")
