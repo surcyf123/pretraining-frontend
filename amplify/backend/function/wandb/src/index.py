eb2d78b2af5d7a07b1d789c5c629276aa0052cbd
maneetgoyal <mgoyal@manufacanalytics.com>
2023-11-24 12:25:57 +0530

    update lambda

--- amplify/backend/function/wandb/src/index.py
+++ amplify/backend/function/wandb/src/index.py
@@ -1,15 +1,16 @@
 import json
+import os
+from utils.upload import upload
 
 def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
+   path=os.path.join(os.getcwd(),"multi.json")
+   upload(path)  
+   return {
       'statusCode': 200,
       'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+    },
+    'body': json.dumps('Hello from your new Amplify Python lambda!')
   }
