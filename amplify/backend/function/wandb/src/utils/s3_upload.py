9d52cbfc462adcaacc085f11bdf440dfd49a6e3d
maneetgoyal <mgoyal@manufacanalytics.com>
2023-11-24 11:35:16 +0530

    Refactor: docs

--- amplify/backend/function/wandb/src/utils/s3_upload.py
+++ amplify/backend/function/wandb/src/utils/s3_upload.py
@@ -3,6 +3,7 @@ import os
 
 s3_client = boto3.client('s3')
 
+# Ref: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html
 def upload(path):
     s3_client.upload_file(
     Filename=path,
