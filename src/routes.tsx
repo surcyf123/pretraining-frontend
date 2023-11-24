fb7b0ee528102a3f27543817851a047b6edd8d4a
maneetgoyal <mgoyal@manufacanalytics.com>
2023-11-24 16:10:24 +0530

    refactor: rename

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import { App } from "./app";
-import { ProtectedRoute } from "./components/ProtectedRoutes";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 import { Dashboard } from "./pages/Dashboard";
 import { ErrorPage } from "./pages/ErrorPage";
 import { Login } from "./pages/Login";
