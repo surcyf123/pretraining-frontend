1e56b34d6f6a2f54c5efe0c3eb4a29b504e98e00
maneetgoyal <mgoyal@manufacanalytics.com>
2023-11-24 12:08:00 +0530

    feat: add storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,15 @@
 export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "pretraining44239c45": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
   "function": {
     "wandb": {
       "Arn": "string",
@@ -6,6 +17,12 @@ export type AmplifyDependentResourcesAttributes = {
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
       "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "s359bce836": {
+      "BucketName": "string",
       "Region": "string"
     }
   }
