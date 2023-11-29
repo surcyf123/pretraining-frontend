ede448c7b504bb42401e7671473c59b20082b067
maneetgoyal <mgoyal@manufacanalytics.com>
2023-11-29 12:14:24 +0530

    revert: state

--- src/sample-data/state.ts
+++ src/sample-data/state.ts
@@ -1,6 +1,6 @@
 import MultiJSONData from "./multi.json";
 import type { RunDetails } from "./interfaces";
 
-const MultiJSON = MultiJSONData as Record<string, RunDetails[]>;
+export const MultiJSON = MultiJSONData as Record<string, RunDetails[]>;
 
 export const Data = Object.values(MultiJSON).flat();
