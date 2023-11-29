491436710231066cd6dbea1acfc7839fdf70bdb5
maneetgoyal <mgoyal@manufacanalytics.com>
2023-11-29 10:52:21 +0530

    Refactor: minor

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,9 +2,9 @@ import { downloadData } from "aws-amplify/storage";
 import type { RunDetails } from "../sample-data/interfaces";
 
 // eslint-disable-next-line import/no-unused-modules
-export async function fetchMulitJSON(): Promise<Record<string, RunDetails[]>> {
+export async function fetchMulitJSON(): Promise<Record<string, (RunDetails | null)[]>> {
   const downloadResult = await downloadData({ key: "multi.json" }).result;
   const text = await downloadResult.body.text();
-  const json = JSON.parse(text) as Record<string, RunDetails[]>;
+  const json = JSON.parse(text) as Record<string, (RunDetails | null)[]>;
   return json;
 }
