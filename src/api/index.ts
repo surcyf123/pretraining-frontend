cc96b8d5fcae6a3e9d18667e87a284ae593f82c9
maneetgoyal <mgoyal@manufacanalytics.com>
2023-11-29 09:59:24 +0530

    Refactor: minor

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,8 @@ import type { RunDetails } from '../sample-data/interfaces';
 
 // eslint-disable-next-line import/no-unused-modules
 export async function fetchMulitJSON():  Promise<Record<string, RunDetails[]>>{
-  const downloadResult = await downloadData({ key: "multi.json" }).result;
-  const json = await downloadResult.body.json() as Record<string, RunDetails[]>;
+  const downloadResult = await downloadData({ key: "multi.json"  }).result;
+  const text = await downloadResult.body.text();
+  const json=JSON.parse(text) as Record<string, RunDetails[]>
   return json
 }
