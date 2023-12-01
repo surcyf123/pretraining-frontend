5ab88f9a101c6281108fd88d3e38bfaa73b80d30
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-01 11:20:30 +0530

    feat: remove hosting

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -20,18 +20,6 @@ export type AmplifyDependentResourcesAttributes = {
       "Region": "string"
     }
   },
-  "hosting": {
-    "S3AndCloudFront": {
-      "CloudFrontDistributionID": "string",
-      "CloudFrontDomainName": "string",
-      "CloudFrontOriginAccessIdentity": "string",
-      "CloudFrontSecureURL": "string",
-      "HostingBucketName": "string",
-      "Region": "string",
-      "S3BucketSecureURL": "string",
-      "WebsiteURL": "string"
-    }
-  },
   "storage": {
     "s359bce836": {
       "BucketName": "string",
