74683d51c0ee4a99b8fbddb0eac36f69e39005a7
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-05 12:02:43 +0530

    update font family

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { Routers } from "./routes";
-import "@fontsource/ruda/400.css";
-import "@fontsource/ruda/500.css";
-import "@fontsource/ruda/600.css";
-import "@fontsource/ruda/700.css";
-import "@fontsource/ruda/800.css";
-import "@fontsource/ruda/900.css";
 import "@fontsource/adlam-display/400.css";
+import "@fontsource/space-mono/400.css";
 
 const MantineTheme = createTheme({
-  fontFamily: "ADLaM Display",
+  fontFamily: "Space Mono, sans-serif",
+  headings: { fontFamily: "ADLaM Display, sans-serif" }, // Ref : https://mantine.dev/theming/typography/#change-fonts
 }); // Ref: https://mantine.dev/theming/theme-object/#store-theme-override-object-in-a-variable
 
 const root = createRoot(document.getElementById("root") as HTMLDivElement);
