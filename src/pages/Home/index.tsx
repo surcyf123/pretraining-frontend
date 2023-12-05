b54329c27c3022e8778d5c092d10b5bcc8a51b3a
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-05 15:38:23 +0530

    Refactor : remove mt

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -68,7 +68,7 @@ export function Home(): JSX.Element {
           Dataset. Finding the best model and delta at the earliest timestamp ensures the most
           incentive.
         </Text>
-        <Text mt="lg">
+        <Text>
           You can view the entire validation system by reading the code in{" "}
           <Pill>neurons/validator.py</Pill>. Pseudocode for the validation system is as follows:
         </Text>
@@ -109,22 +109,30 @@ export function Home(): JSX.Element {
               `}
         </Code>
         <Text>
-            The behaviour of <Pill>iswin( loss_a, loss_b, timestamp_a, timestamp_b)</Pill> function intentionally skews the win function to reward models which have been hosted earlier such that newer models are only better than others iff their loss is <Pill>epsilon</Pill> percent lower accoring to the following function. Currently <Pill>epsilon</Pill> is set to 1% and is a hyper parameter of the mechanism
-          </Text>
-          <Code block>
-            {
-              `
+          The behaviour of <Pill>iswin( loss_a, loss_b, timestamp_a, timestamp_b)</Pill> function
+          intentionally skews the win function to reward models which have been hosted earlier such
+          that newer models are only better than others iff their loss is <Pill>epsilon</Pill>{" "}
+          percent lower accoring to the following function. Currently <Pill>epsilon</Pill> is set to
+          1% and is a hyper parameter of the mechanism
+        </Text>
+        <Code block>
+          {`
               def iswin( loss_a, loss_b, timestamp_a, timestamp_b, epsilon ):
               loss_a = (1 - epsilon) * loss_a if timestamp_a < timestamp_b else loss_a
               loss_b = (1 - epsilon) * loss_b if timestamp_b < timestamp_a else loss_b
               if loss_a < loss_b: return True
               else: return False
-              `
-            }
-          </Code>
-          <Text>
-          It is important to note that this affects the game theoretics of the incentive landscape since miners should only update their model (thus updating their timestamp to a newer date) if they have achieved an <Pill>epsilon</Pill> better loss on average on the Falcon Refined Web dataset than their previous model. This undermines the obvious optimal strategy for miners to copy the publicly available models from other miners. They can and should copy other miners, but they will always obtain fewer wins compared to them until they also decrease their loss by <Pill>epsilon</Pill>.
-          </Text>
+              `}
+        </Code>
+        <Text>
+          It is important to note that this affects the game theoretics of the incentive landscape
+          since miners should only update their model (thus updating their timestamp to a newer
+          date) if they have achieved an <Pill>epsilon</Pill> better loss on average on the Falcon
+          Refined Web dataset than their previous model. This undermines the obvious optimal
+          strategy for miners to copy the publicly available models from other miners. They can and
+          should copy other miners, but they will always obtain fewer wins compared to them until
+          they also decrease their loss by <Pill>epsilon</Pill>.
+        </Text>
       </Stack>
     </Container>
   );
