5623b7714b25f54383a18fb2c64786db0a951cd6
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-06 18:10:15 +0530

    Refactor : delete unused files

--- src/sample-data/state.ts
+++
@@ -1,6 +0,0 @@
-import MultiJSONData from "./multi.json";
-import type { RunDetails } from "./interfaces";
-
-const MultiJSON = MultiJSONData as Record<string, RunDetails[]>;
-
-export const Data = Object.values(MultiJSON).flat();
