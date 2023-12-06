d69dd288cb387de653247b0d77ce40b5bbc2d88f
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-06 16:09:57 +0530

    add params in story

--- src/charts/BestLossChart/index.stories.tsx
+++ src/charts/BestLossChart/index.stories.tsx
@@ -1,9 +1,19 @@
+import MultiJSONData from "../../sample-data/multi.json";
 import { BestLossChart } from ".";
+import type { RunDetails } from "../../sample-data/interfaces";
 import type { Meta, StoryObj } from "@storybook/react";
 
 export default {
   title: "BestLossChart",
   component: BestLossChart,
+  args: {
+    data: MultiJSONData as Record<string, RunDetails[]>,
+    yAxis: "best_average_loss",
+    xAxis: "timestamp",
+    yAxisTitle: "Best average loss",
+    xAxisTitle: "Time",
+    style: { height: "100vh" },
+  },
 } as Meta<typeof BestLossChart>;
 
 export const Template: StoryObj<typeof BestLossChart> = {};
