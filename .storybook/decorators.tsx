5245690371a87dc3c9ef301277f189385668b575
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-06 16:20:24 +0530

    fix header story not getting render

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -5,13 +5,16 @@ import { MantineProvider } from "@mantine/core";
 import type { MantineProviderProps } from "@mantine/core";
 import "@mantine/core/styles.css"; //Ref: https://mantine.dev/styles/global-styles/
 import { BrowserRouter } from "react-router-dom";
+import { Authenticator } from "@aws-amplify/ui-react";
 
 function ThemeWrapper({ children }: MantineProviderProps): JSX.Element {
   const darkMode = useDarkMode();
   return (
-    <MantineProvider forceColorScheme={darkMode === true ? "dark" : "light"}>
-      {children}
-    </MantineProvider>
+    <Authenticator.Provider>
+      <MantineProvider forceColorScheme={darkMode === true ? "dark" : "light"}>
+        {children}
+      </MantineProvider>
+    </Authenticator.Provider>
   );
 }
 
