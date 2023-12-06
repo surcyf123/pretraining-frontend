6a19daf9bd49e9a5b1850ffd6a24c4d1e93bf3eb
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-06 19:05:41 +0530

    refactor: update font size map

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,7 @@ const MantineTheme = createTheme({
   fontFamily: "Space Mono, sans-serif",
   fontFamilyMonospace: "Space Mono, Courier, monospace", // Ref : https://mantine.dev/theming/typography/#change-fonts
   headings: { fontFamily: "ADLaM Display, sans-serif" }, // Ref : https://mantine.dev/theming/typography/#change-fonts
+  fontSizes: { xs: "14px", sm: "16px", md: "18px", lg: "20px", xl: "22px" },
 }); // Ref: https://mantine.dev/theming/theme-object/#store-theme-override-object-in-a-variable
 
 const root = createRoot(document.getElementById("root") as HTMLDivElement);
