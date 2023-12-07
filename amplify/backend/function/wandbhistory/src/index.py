0ef1e9f6cdbff027979778b34853542e872b0cdf
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-07 11:57:01 +0530

    feat: clone wandb function

--- amplify/backend/function/wandbhistory/src/index.py
+++ amplify/backend/function/wandbhistory/src/index.py
@@ -1,15 +1,17 @@
 import json
+from utils.upload import upload
+from utils.script import init_wandb, calculate_best_average_loss
 
 def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+    run_data = init_wandb()
+    history_run_data = run_data.get("history",{})
+    recent_run_data = run_data.get("recent",{})
+    history = calculate_best_average_loss(history_run_data)
+    recent = calculate_best_average_loss(recent_run_data)
+    upload(recent, filename = "recent.json")
+    upload(history, filename = "history.json")
+    
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Multi.json file updated successfully."),
+    }
---
+++ amplify/backend/function/wandbhistory/src/utils/script.py
@@ -0,0 +1,94 @@
+import wandb
+import datetime
+import json
+import pandas as pd
+import math
+
+wandb.login()
+api = wandb.Api()
+
+project_name = "pretraining-subnet"
+entity_name = "opentensor-dev"
+
+runs = api.runs(f"{entity_name}/{project_name}")
+now = datetime.datetime.now()
+
+def replace_inf_nan(obj):
+    if isinstance(obj, list):
+        return [replace_inf_nan(item) for item in obj]
+    elif isinstance(obj, dict):
+        return {key: replace_inf_nan(value) for key, value in obj.items()}
+    # in python 'inf' (positive infinity) is instance of float. Ref: https://www.geeksforgeeks.org/python-infinity/
+    # obj == float('inf') checks if obj is positive infinity.
+    elif isinstance(obj, float) and (math.isinf(obj) or math.isnan(obj)):
+        return None
+    else:
+        return obj
+
+def calculate_best_average_loss(data):
+    if(isinstance(data, dict)):
+        for items in data.values():
+            for item in items:
+                if(isinstance(item, dict)):
+                    uids = item.get("uids", [])
+                    uid_data = item.get("uid_data", {})
+                    if(isinstance(uids, list) and isinstance(uid_data, dict)):
+                        average_losses = []
+                        for uid in uids:
+                            current_uid_data = uid_data.get(str(uid), None)
+                            if (isinstance(current_uid_data, dict)):
+                                average_loss = current_uid_data.get("average_loss", None)
+                                if(isinstance(average_loss, float) or isinstance(average_loss, int)):
+                                    average_losses.append(average_loss)
+                        if(len(average_losses) > 0):
+                            best_average_loss = min(average_losses)
+                            item["best_average_loss"] = best_average_loss
+    return data  
+
+def init_wandb():
+  all_run_data = {}
+  recent_run_data={}
+
+  for run in runs:
+      # Parse the created_at time
+      try:
+          created_at = datetime.datetime.strptime(run.created_at, "%Y-%m-%dT%H:%M:%S")
+      except ValueError:
+          # Handle possible different time formats
+          created_at = datetime.datetime.strptime(run.created_at, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+      # Calculate the time difference in days
+      time_diff = now - created_at
+
+      # Check if the run was started less than 3 days ago and "validator" is in the run name
+      if  time_diff.days < 7 and "validator" in run.name:
+          print(f"Processing run: {run.name}")
+
+          # Retrieve the run history
+          run_data = run.history()
+
+          # Extract the 'original_format_json' key
+          if 'original_format_json' in run_data.columns:
+              original_format_json_data = run_data['original_format_json']
+
+              # Convert the Pandas Series to a list or dict
+              if (isinstance(original_format_json_data, pd.Series)):
+                  converted_data = []
+                  target_list = original_format_json_data.to_list()
+                  for ele in target_list:
+                      if isinstance(ele, str):
+                          converted_data.append(json.loads(ele))
+                      else:    
+                          converted_data.append(ele)
+              else:
+                  converted_data = original_format_json_data
+
+              # Replace NaN value and infinity values with null
+              converted_data = replace_inf_nan(converted_data)
+              all_run_data[run.name] = converted_data
+              if time_diff.days < 3:
+                  recent_run_data[run.name] = converted_data
+  return {
+      "recent": recent_run_data,
+      "history": all_run_data
+  }
