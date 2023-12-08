4c2deebcd320f9d3a53f7b4ce0c718395eb48953
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-08 13:34:06 +0530

    feat: sort elements by time stamp

--- amplify/backend/function/wandbHistory/src/index.py
+++ amplify/backend/function/wandbHistory/src/index.py
@@ -5,7 +5,8 @@ from utils.script import init_wandb, calculate_best_average_loss
 def handler(event, context):
     all_run_data = init_wandb()
     output = calculate_best_average_loss(all_run_data)
-    upload(output, filename = "history.json")
+    sorted_output=sorted(output, key = lambda ele  : ele["timestamp"]) # Ref: https://stackoverflow.com/questions/72899/how-to-sort-a-list-of-dictionaries-by-a-value-of-the-dictionary-in-python
+    upload(sorted_output, filename = "history.json")
 
     return {
         "statusCode": 200,
