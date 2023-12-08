aa9d3569836216a84d771a9179090b42add40681
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-08 15:05:06 +0530

    Refactor: minor

--- src/charts/BestLossChart/index.tsx
+++ src/charts/BestLossChart/index.tsx
@@ -68,11 +68,7 @@ export function BestLossChart({
 }: BestLossChartProps): JSX.Element {
   const chartRef = useRef<HTMLDivElement>(null);
 
-  const processedData = useMemo(() => {
-    const formatTimeStamp = data.map((ele) => ({ ...ele, timestamp: ele.timestamp * 1000 }));
-    const output = group(formatTimeStamp, (d) => d.key);
-    return output;
-  }, [data]);
+  const processedData = useMemo(() => group(data, (d) => d.key), [data]);
 
   useEffect(() => {
     let chart: ECharts | undefined;
