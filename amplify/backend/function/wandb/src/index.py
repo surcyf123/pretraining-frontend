6eee36546a1e362b47176613421a777fdcf99096
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-12 12:22:12 +0530

    Feat: update inf nan logic

--- amplify/backend/function/wandb/src/index.py
+++ amplify/backend/function/wandb/src/index.py
@@ -6,7 +6,6 @@ def handler(event, context):
     recent_run_data = init_wandb()
     recent = calculate_best_average_loss(recent_run_data)
     upload(recent, filename = "recent.json")
-
     return {
         "statusCode": 200,
         "body": json.dumps("recent.json file updated successfully."),
