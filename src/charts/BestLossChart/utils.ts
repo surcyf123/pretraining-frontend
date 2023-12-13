8f04a2a93dce74d68673facf6faf45501fa74cd0
Maneet Goyal <mgoyal@manufacanalytics.com>
2023-12-13 15:35:31 +0530

    Update src/charts/BestLossChart/utils.ts

--- src/charts/BestLossChart/utils.ts
+++ src/charts/BestLossChart/utils.ts
@@ -2,7 +2,6 @@ import { scaleOrdinal } from "d3-scale";
 import { schemeBrBG } from "d3-scale-chromatic";
 import type { ScaleOrdinal } from "d3-scale";
 
-// eslint-disable-next-line import/no-unused-modules
 export function colorGenerator(): ScaleOrdinal<string, string> {
   const generator = scaleOrdinal(schemeBrBG[11]);
   return generator;
