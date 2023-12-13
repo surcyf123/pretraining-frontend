5ca81353acb27ee25cb7a90991b07621fc3dbe2b
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-13 17:35:51 +0530

    Update Component

--- src/components/TopBar/index.stories.tsx
+++ src/components/TopBar/index.stories.tsx
@@ -5,13 +5,22 @@ import type { Meta, StoryObj } from "@storybook/react";
 export default {
   title: "Topbar",
   component: TopBar,
+  args: {
+    properties: {
+      averageLoss: 120,
+      averageVTrust: 23456,
+      bestUID: 123,
+      minLoss: 0,
+      subnetEmission: 3456,
+    },
+  },
 } as Meta<typeof TopBar>;
 
 export const Template: StoryObj<typeof TopBar> = {
-  render: function Wrapper() {
+  render: function Wrapper(args) {
     return (
       <AppShell>
-        <TopBar />
+        <TopBar {...args} />
       </AppShell>
     );
   },
--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,3 +1,30 @@
-export function TopBar(): JSX.Element {
-  return <>Hello World</>;
+import { Group, Image, Stack, Text } from "@mantine/core";
+import Logo from "../Header/logo.png";
+
+const Properties = {
+  minLoss: "Min loss",
+  averageLoss: "Average loss",
+  bestUID: "Best UID",
+  averageVTrust: "Average Vtrust",
+  subnetEmission: "Subnet Emission",
+} as const;
+
+export interface Data {
+  properties: Record<keyof typeof Properties, number>;
+}
+
+export function TopBar({ properties }: Data): JSX.Element {
+  return (
+    <Group justify="space-between">
+      <Image {...Logo} h={30} alt="Openpretrain logo" />
+      <Group align="center">
+        {Object.entries(properties).map(([key, value]) => (
+          <Stack key={key}>
+            <Text fw="bold">{Properties[key as keyof typeof Properties]}</Text>
+            <Text>{value}</Text>
+          </Stack>
+        ))}
+      </Group>
+    </Group>
+  );
 }
