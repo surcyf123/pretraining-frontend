4797e772322af24b87e541d68931b504f729c5d3
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-13 16:38:48 +0530

    Refactor: minor

--- src/charts/BestLossChart/utils.ts
+++ src/charts/BestLossChart/utils.ts
@@ -3,6 +3,6 @@ import { schemeBrBG } from "d3-scale-chromatic";
 import type { ScaleOrdinal } from "d3-scale";
 
 export function colorGenerator(): ScaleOrdinal<string, string> {
-  const generator = scaleOrdinal(schemeBrBG[11]);
+  const generator = scaleOrdinal(schemeBrBG[11]); // Ref: https://d3js.org/d3-scale-chromatic/diverging#schemePiYG
   return generator;
 }
