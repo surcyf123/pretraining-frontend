2ee97513b9c7d7407cd3c4081a757bd1ddfd0dde
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-16 14:43:15 +0530

    refactor: minor

--- amplify/backend/function/metagraph/src/utils/metagraph.py
+++ amplify/backend/function/metagraph/src/utils/metagraph.py
@@ -6,16 +6,16 @@ metagraph = bt.metagraph(netuid=9, sync=True)  # Keeping default network "finey"
 def get_metagraph_data():
   metagraph_data={
   "metaData":metagraph.metadata(),
-  "each_neuron_stake":metagraph.S, # Ref: https://docs.bittensor.com/python-api/html/autoapi/bittensor/metagraph/index.html#bittensor.metagraph.metagraph.S
-  "each_neuron_ranks":metagraph.R,
-  "each_neuron_incentives":metagraph.I,
-  "each_neuron_emission":metagraph.E,
-  "each_neuron_consensus":metagraph.C,
-  "each_neuron_trust":metagraph.T,
-  "each_neuron_validator_trust":metagraph.Tv,
-  "each_neuron_dividends":metagraph.D,
-  "bonds":metagraph.B,
-  "each_neuron_weight":metagraph.W,
+  # "each_neuron_stake":metagraph.S, # Ref: https://docs.bittensor.com/python-api/html/autoapi/bittensor/metagraph/index.html#bittensor.metagraph.metagraph.S
+  "each_neuron_ranks":metagraph.R.tolist(),
+  "each_neuron_incentives":metagraph.I.tolist(),
+  "each_neuron_emission":metagraph.E.tolist(),
+  "each_neuron_consensus":metagraph.C.tolist(),
+  "each_neuron_trust":metagraph.T.tolist(),
+  "each_neuron_validator_trust":metagraph.Tv.tolist(),
+  "each_neuron_dividends":metagraph.D.tolist(),
+  "bonds":metagraph.B.tolist(),
+  "each_neuron_weight":metagraph.W.tolist(),
   "each_neuron_hotkeys":metagraph.hotkeys,
   "each_neuron_coldkeys":metagraph.coldkeys,
   }
