049118d59a98d07769890efa2151ae7f557d01cf
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-18 20:43:06 +0530

    feat: Add skeleton

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,18 +1,21 @@
-import { Group, Stack, Text } from "@mantine/core";
+import { Group, Skeleton, Stack, Text } from "@mantine/core";
 
 export interface TopBarProps {
   metrics: Record<string, string | number | undefined | null>;
+  loading?: boolean;
 }
 
-export function TopBar({ metrics }: TopBarProps): JSX.Element {
+export function TopBar({ metrics, loading }: TopBarProps): JSX.Element {
   return (
-    <Group wrap="nowrap" justify="space-between">
-      {Object.entries(metrics).map(([key, value]) => (
-        <Stack key={key} align="center">
-          <Text fw="bold">{key}</Text>
-          <Text>{value}</Text>
-        </Stack>
-      ))}
-    </Group>
+    <Skeleton visible={loading ?? false}>
+      <Group wrap="nowrap" justify="space-between">
+        {Object.entries(metrics).map(([key, value]) => (
+          <Stack key={key} align="center">
+            <Text fw="bold">{key}</Text>
+            <Text>{value}</Text>
+          </Stack>
+        ))}
+      </Group>
+    </Skeleton>
   );
 }
