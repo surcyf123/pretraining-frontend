cb886c7ec88eafdf62b480149063e870ddc4f282
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-20 17:08:37 +0530

    refactor: rename

--- bittensor/src/utils/metagraph.py
+++ bittensor/src/utils/metagraph.py
@@ -1,24 +1,29 @@
 import bittensor as bt
 import pandas as pd
 
-# Ref: https://docs.bittensor.com/python-api/html/autoapi/bittensor/metagraph/index.html
-metagraph = bt.metagraph(netuid=9, sync=True,lite=False)  # Keeping default network "finey" Ref: https://docs.bittensor.com/python-api/html/autoapi/bittensor/metagraph/index.html#bittensor.metagraph.metagraph
+# Ref: https://docs.bittensor.com/python-api/html/autoapi/bittensor/metagraph/index.html#bittensor.metagraph.metagraph
+metagraph = bt.metagraph(
+  netuid=9,
+  sync=True, 
+  lite=False
+)
 
 def get_metagraph_data():
   metadata = metagraph.metadata(),
   neuron_data = {
-  "each_neuron_stake": metagraph.S.tolist(), # Ref: https://docs.bittensor.com/python-api/html/autoapi/bittensor/metagraph/index.html#bittensor.metagraph.metagraph.S
-  "each_neuron_ranks": metagraph.R.tolist(),
-  "each_neuron_incentives": metagraph.I.tolist(),
-  "each_neuron_emission": metagraph.E.tolist(),
-  "each_neuron_consensus": metagraph.C.tolist(),
-  "each_neuron_trust": metagraph.T.tolist(),
-  "each_neuron_validator_trust": metagraph.Tv.tolist(),
-  "each_neuron_dividends": metagraph.D.tolist(),
+  "stake": metagraph.S.tolist(),
+  "rank": metagraph.R.tolist(),
+  "incentive": metagraph.I.tolist(),
+  "emission": metagraph.E.tolist(),
+  "consensus": metagraph.C.tolist(),
+  "trust": metagraph.T.tolist(),
+  "trust": metagraph.Tv.tolist(),
+  "dividends": metagraph.D.tolist(),
   "bonds": metagraph.B.tolist(),
-  "each_neuron_weight": metagraph.W.tolist(),
-  "each_neuron_hotkeys": metagraph.hotkeys,
-  "each_neuron_coldkeys": metagraph.coldkeys,
+  "weight": metagraph.W.tolist(),
+  "hotkey": metagraph.hotkeys,
+  "coldkey": metagraph.coldkeys,
+  "address":metagraph.addresses()
   }
 
   output = pd.DataFrame(neuron_data).to_dict(orient="records") # transform data to array of records
