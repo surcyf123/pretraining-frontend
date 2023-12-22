786e67d3393d64383951142089c2e49d038cb2ec
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-22 14:39:14 +0530

    Add story

---
+++ src/components/Taostats/index.stories.tsx
@@ -0,0 +1,9 @@
+import { TaoStats } from ".";
+import type { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "TaoStats",
+  component: TaoStats,
+} as Meta<typeof TaoStats>;
+
+export const Template: StoryObj<typeof TaoStats> = {};
