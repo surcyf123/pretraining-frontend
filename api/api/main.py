04e5aa9d1d5234580970780eaac669c0e8eccd6e
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-26 14:00:35 +0530

    feat: Add start and serve scripts

--- api/api/main.py
+++ api/api/main.py
@@ -7,5 +7,8 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+def start():
+    uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True)
+
 def serve():
-    uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True)    
+    uvicorn.run("api.main:app", host="0.0.0.0", port=8000)          
