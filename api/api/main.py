607b10dc012a71e4470c6ffb6b9a2762ad8311d4
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-26 16:50:24 +0530

    refactor: minor

--- api/api/main.py
+++ api/api/main.py
@@ -1,16 +1,34 @@
 from fastapi import FastAPI
+from typing import Dict
+import json
 import uvicorn
 import bittensor
 
 app = FastAPI()
+metagraphs: Dict[int, bittensor.metagraph] = dict()
+
+def get_from_cache(netuid: int = 0):
+    metagraph = metagraphs.get(netuid)
+    if metagraph is None:
+        metagraph = bittensor.metagraph(netuid)
+        metagraphs[netuid] = metagraph
+    else:
+        metagraph.sync()
+    return metagraph
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/metagraphs/{netuid}")
-def metagraph(netuid: int = 0):
-    return bittensor.metagraph(netuid).metadata
+@app.get("/metadata/{netuid}")
+def metadata(netuid: int = 0):
+    metagraph = get_from_cache(netuid)
+    return json.dumps(metagraph.metadata)
+
+@app.get("/weights/{netuid}")
+def weights(netuid: int = 0):
+    metagraph = get_from_cache(netuid)
+    return json.dumps(metagraph.weights)
 
 def start():
     uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True, loop="asyncio") # Ref: Why asyncio loop? https://youtrack.jetbrains.com/issue/PY-57332
