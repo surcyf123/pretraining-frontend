5b2b66339534d5fda8ddd201df34d78a756846b2
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-27 19:58:54 +0530

    refactor: relocate

--- src/pages/General/index.tsx
+++ src/pages/General/index.tsx
@@ -1,4 +1,6 @@
-import { Divider, Stack, Text } from "@mantine/core";
+import { Card, Divider, Stack, Text } from "@mantine/core";
+import { Heatmap } from "../../charts/Heatmap";
+import { HeatmapData } from "../../charts/Heatmap/data";
 import { TaoStats } from "../../components/TaoStats";
 import { ValidatorTable } from "../../components/ValidatorTable";
 import { Validators } from "../../components/ValidatorTable/utils";
@@ -9,6 +11,18 @@ export function General(): JSX.Element {
       <TaoStats />
       <Divider />
       <ValidatorTable data={Validators} />
+      <Card shadow="md">
+        <Heatmap
+          title="Weight Matrix"
+          style={{ height: "50vh" }}
+          data={HeatmapData}
+          xAxis="minerID"
+          yAxis="validatorID"
+          visualAxis="weight"
+          xAxisLabel="Miner"
+          yAxisLabel="Validator"
+        />
+      </Card>
       <Text ta="center">Comming soon...</Text>
     </Stack>
   );
