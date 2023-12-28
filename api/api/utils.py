26c89663994817ba732a48e364da05c7eb519bbb
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-28 16:12:12 +0530

    fix: update logic

--- api/api/utils.py
+++ api/api/utils.py
@@ -3,6 +3,5 @@ from torch import FloatTensor, Tensor
 
 # Ref: https://docs.bittensor.com/emissions#trust
 def calculate_trust(W: FloatTensor, S: FloatTensor, threshold: int = 0) -> Tensor:
-    Sn = (S/ S.sum()).clone().float()
     Wn = (W > threshold).float()
-    return Wn.T @ Sn
+    return Wn.T @ S
