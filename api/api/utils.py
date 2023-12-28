fad1c2967a8aa82fc4443133b51b78e086537242
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-28 15:52:54 +0530

    style: pretty

--- api/api/utils.py
+++ api/api/utils.py
@@ -2,6 +2,6 @@ from torch import FloatTensor, Tensor
 
 
 # Ref: https://docs.bittensor.com/emissions#trust
-def calculate_trust(W: FloatTensor, S:Tensor, threshold: int = 0)->Tensor:
+def calculate_trust(W: FloatTensor, S: Tensor, threshold: int = 0) -> Tensor:
     Wn = (W > threshold).float()
     return Wn.T @ S
