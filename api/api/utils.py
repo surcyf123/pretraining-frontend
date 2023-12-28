868e807fe2433066d198ad8ff04ff3681a6a6284
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-28 16:12:53 +0530

    Revert "fix: update logic"
    This reverts commit 26c89663994817ba732a48e364da05c7eb519bbb.

--- api/api/utils.py
+++ api/api/utils.py
@@ -3,5 +3,6 @@ from torch import FloatTensor, Tensor
 
 # Ref: https://docs.bittensor.com/emissions#trust
 def calculate_trust(W: FloatTensor, S: FloatTensor, threshold: int = 0) -> Tensor:
+    Sn = (S/ S.sum()).clone().float()
     Wn = (W > threshold).float()
-    return Wn.T @ S
+    return Wn.T @ Sn
