224dbe686b5773533a2c1343bd7d6de6c6c9861b
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-29 15:09:36 +0530

    add subnet tab

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import { useAuthenticator } from "@aws-amplify/ui-react";
 import { AppShell, ActionIcon, Group, NavLink as MantineNavLink, Image } from "@mantine/core";
-import { IconLogout, IconMoonStars, IconSun } from "@tabler/icons-react";
+import { IconChevronDown, IconLogout, IconMoonStars, IconSun } from "@tabler/icons-react";
 import { NavLink, useLocation } from "react-router-dom";
 import Logo from "./logo.png";
 import type { MantineColorScheme } from "@mantine/core";
@@ -23,26 +23,38 @@ const NavLinks: { path: string; label: string }[] = [
     path: "/dashboard",
     label: "Dashboard",
   },
+  {
+    path: "#",
+    label: "Subnets",
+  },
 ];
 
 export function Header({ colorScheme, onToggleColorScheme }: HeaderProps): JSX.Element {
   const location = useLocation();
   const { authStatus, signOut } = useAuthenticator((context) => [context.authStatus]);
-
   return (
     <AppShell.Header p="xs">
       <Group justify="space-between" align="center">
         <Image {...Logo} h={30} alt="Openpretrain logo" />
         <Group wrap="nowrap">
-          {NavLinks.map(({ path, label }) => (
-            <MantineNavLink
-              key={path}
-              component={NavLink}
-              active={location.pathname === path}
-              to={path}
-              label={label}
-            />
-          ))}
+          {NavLinks.map(({ path, label }) =>
+            label === "Subnets" ? (
+              <MantineNavLink
+                rightSection={<IconChevronDown size={20} />}
+                to="#"
+                component={NavLink}
+                label="Subnets"
+              />
+            ) : (
+              <MantineNavLink
+                key={path}
+                component={NavLink}
+                active={location.pathname === path}
+                to={path}
+                label={label}
+              />
+            ),
+          )}
         </Group>
         <Group>
           <ActionIcon onClick={onToggleColorScheme} variant="default">
