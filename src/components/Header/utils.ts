4f238614da7b642d54739032c5f6dda28151b849
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-29 15:03:15 +0530

    create subnet data

--- src/components/Header/utils.ts
+++ src/components/Header/utils.ts
@@ -2,3 +2,140 @@
 export function sum(a: number, b: number) {
   return a + b;
 }
+
+export const SubnetsData = [
+  {
+    label: "00 : Root",
+    path: "#00",
+  },
+  {
+    label: "01 : Text Prompting",
+    path: "#01",
+  },
+  {
+    label: "02 : Machine Transation",
+    path: "#02",
+  },
+  {
+    label: "03 : Data Scraping",
+    path: "#03",
+  },
+  {
+    label: "04 : Multi Modality",
+    path: "#04",
+  },
+  {
+    label: "05 : Image Generation",
+    path: "#05",
+  },
+  {
+    label: "06 : Unknown",
+    path: "#06",
+  },
+  {
+    label: "07 : Storage",
+    path: "#07",
+  },
+  {
+    label: "08 : Time Series Prediction",
+    path: "#08",
+  },
+  {
+    label: "09 : Pretraining",
+    path: "#09",
+  },
+  {
+    label: "10 : Map Reduce",
+    path: "#10",
+  },
+
+  {
+    label: "11 : Text Training",
+    path: "#11",
+  },
+  {
+    label: "12 : Unknown",
+    path: "#12",
+  },
+  {
+    label: "13 : Dataverse",
+    path: "#13",
+  },
+  {
+    label: "14 : LLM Defender",
+    path: "#14",
+  },
+  {
+    label: "15 : Blockchain Insights",
+    path: "#15",
+  },
+  {
+    label: "16 : Audio",
+    path: "#16",
+  },
+  {
+    label: "17 : Petal",
+    path: "#17",
+  },
+  {
+    label: "18 : Cortex.t",
+    path: "#18",
+  },
+  {
+    label: "19 : Vision",
+    path: "#19",
+  },
+  {
+    label: "20 : Unknown",
+    path: "#20",
+  },
+  {
+    label: "21 : Filetao",
+    path: "#21",
+  },
+
+  {
+    label: "22 : Unknown",
+    path: "#22",
+  },
+  {
+    label: "23 : Prime-Net",
+    path: "#23",
+  },
+  {
+    label: "24 : Unknown",
+    path: "#24",
+  },
+  {
+    label: "25 : Bitcurrent",
+    path: "#25",
+  },
+  {
+    label: "26 : Image Alchemy",
+    path: "#26",
+  },
+  {
+    label: "27 : Compute",
+    path: "#27",
+  },
+  {
+    label: "28 : ZK Tensor",
+    path: "#17",
+  },
+  {
+    label: "29 : 3D Gen",
+    path: "#29",
+  },
+  {
+    label: "30 : Lovelace",
+    path: "#30",
+  },
+  {
+    label: "31 : Game of Life",
+    path: "#31",
+  },
+  {
+    label: "32 : Roleplay",
+    path: "#32",
+  },
+];
