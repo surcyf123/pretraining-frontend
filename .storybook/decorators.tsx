756399f85a2073aace884338c16b904a3f7f088c
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-29 09:36:35 +0530

    Add tanstack provider in storybook

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -6,15 +6,20 @@ import type { MantineProviderProps } from "@mantine/core";
 import "@mantine/core/styles.css"; //Ref: https://mantine.dev/styles/global-styles/
 import { BrowserRouter } from "react-router-dom";
 import { Authenticator } from "@aws-amplify/ui-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const ReactQueryClient = new QueryClient();
 function ThemeWrapper({ children }: MantineProviderProps): JSX.Element {
   const darkMode = useDarkMode();
+
   return (
-    <Authenticator.Provider>
-      <MantineProvider forceColorScheme={darkMode === true ? "dark" : "light"}>
-        {children}
-      </MantineProvider>
-    </Authenticator.Provider>
+    <QueryClientProvider client={ReactQueryClient}>
+      <Authenticator.Provider>
+        <MantineProvider forceColorScheme={darkMode === true ? "dark" : "light"}>
+          {children}
+        </MantineProvider>
+      </Authenticator.Provider>
+    </QueryClientProvider>
   );
 }
 
