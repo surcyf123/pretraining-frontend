263b6f57c623c9ce152a129108113324f45521a2
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-30 12:54:55 +0530

    Refactor : handle children navs link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,13 @@
 import { useAuthenticator } from "@aws-amplify/ui-react";
-import { AppShell, ActionIcon, Group, NavLink as MantineNavLink, Image, Text } from "@mantine/core";
+import {
+  AppShell,
+  ActionIcon,
+  Group,
+  NavLink as MantineNavLink,
+  Image,
+  Menu,
+  Text,
+} from "@mantine/core";
 import { IconChevronDown, IconLogout, IconMoonStars, IconSun } from "@tabler/icons-react";
 import { NavLink, useLocation } from "react-router-dom";
 import Logo from "./logo.png";
@@ -46,17 +54,34 @@ export function Header({ colorScheme, onToggleColorScheme }: HeaderProps): JSX.E
         <Group wrap="nowrap">
           {NavLinks?.map(({ label, link, links }) =>
             Array.isArray(links) === true ? (
-              <MantineNavLink // TODO : handle all child nav links
-                component={NavLink}
-                active={location.pathname === link}
-                to={link}
-                label={<Text fw={500}>{label}</Text>}
-                noWrap
-                title={`Go to ${label}`}
-                rightSection={<IconChevronDown />}
-              />
+              <Menu key={link} trigger="hover">
+                <Menu.Target>
+                  <MantineNavLink
+                    component={NavLink}
+                    active={location.pathname === link}
+                    to={link}
+                    label={<Text fw={500}>{label}</Text>}
+                    noWrap
+                    title={`Go to ${label}`}
+                    rightSection={<IconChevronDown size="1rem" />}
+                  />
+                </Menu.Target>
+                <Menu.Dropdown>
+                  {links?.map((item) => (
+                    <Menu.Item key={item.link}>
+                      <MantineNavLink
+                        component={NavLink}
+                        active={location.pathname === item.link}
+                        to={item.link}
+                        label={<Text>{item.label}</Text>}
+                      />
+                    </Menu.Item>
+                  ))}
+                </Menu.Dropdown>
+              </Menu>
             ) : (
               <MantineNavLink
+                key={link}
                 component={NavLink}
                 active={location.pathname === link}
                 to={link}
