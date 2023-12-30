a270a07dbb11466d47e27a9e3d6d154065703e64
maneetgoyal <mgoyal@manufacanalytics.com>
2023-12-30 12:25:20 +0530

    Refactor : revert

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import { useAuthenticator } from "@aws-amplify/ui-react";
 import { AppShell, ActionIcon, Group, NavLink as MantineNavLink, Image } from "@mantine/core";
-import { IconChevronDown, IconLogout, IconMoonStars, IconSun } from "@tabler/icons-react";
+import { IconLogout, IconMoonStars, IconSun } from "@tabler/icons-react";
 import { NavLink, useLocation } from "react-router-dom";
 import Logo from "./logo.png";
 import type { MantineColorScheme } from "@mantine/core";
@@ -43,24 +43,15 @@ export function Header({ colorScheme, onToggleColorScheme }: HeaderProps): JSX.E
       <Group justify="space-between" align="center">
         <Image {...Logo} h={30} alt="Openpretrain logo" />
         <Group wrap="nowrap">
-          {NavLinks.map(({ path, label }) =>
-            label === "Subnets" ? (
-              <MantineNavLink
-                rightSection={<IconChevronDown size={20} />}
-                to="#"
-                component={NavLink}
-                label="Subnets"
-              />
-            ) : (
-              <MantineNavLink
-                key={path}
-                component={NavLink}
-                active={location.pathname === path}
-                to={path}
-                label={label}
-              />
-            ),
-          )}
+          {NavLinks.map(({ path, label }) => (
+            <MantineNavLink
+              key={path}
+              component={NavLink}
+              active={location.pathname === path}
+              to={path}
+              label={label}
+            />
+          ))}
         </Group>
         <Group>
           <ActionIcon onClick={onToggleColorScheme} variant="default">
