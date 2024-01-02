c35b78ff25fac2da8f519d5604fb44b7a05f42da
maneetgoyal <mgoyal@manufacanalytics.com>
2024-01-02 15:55:01 +0530

    fetch price

--- api/api/main.py
+++ api/api/main.py
@@ -6,7 +6,9 @@ import uvicorn
 import bittensor
 from CacheToolsUtils import cachetools, cached
 from .utils import calculateTrust, calculateRank, calculateEmission, calculateConsensus
+import requests
 
+BaseMEXCEndpoint="https://api.mexc.com"
 app = FastAPI()
 cache = cachetools.TTLCache(
     maxsize=33, ttl=10 * 60
@@ -135,6 +137,14 @@ def vitals():
     )
     vitals = df.to_dict(orient="records")
     return vitals
+
+@app.get("/stats")
+@cached(cache=cache)
+def currentTaoPrice():
+    output={}
+    price = requests.get(f"{BaseMEXCEndpoint}/api/v3/ticker/price",params={"symbol":"TAOUSDT"})
+    output["price"] = price.json(),
+    return output
 
 
 def start():
