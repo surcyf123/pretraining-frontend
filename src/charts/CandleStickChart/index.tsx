0eb528052a9a5706189b859f931a4c1213293f9e
maneetgoyal <mgoyal@manufacanalytics.com>
2024-01-04 15:52:55 +0530

    feat: chart init

---
+++ src/charts/CandleStickChart/index.tsx
@@ -0,0 +1,16 @@
+import { Skeleton } from "@mantine/core";
+import { useRef, type CSSProperties } from "react";
+
+export interface CandleStickChartProps {
+  style?: CSSProperties;
+  loading?: boolean;
+}
+
+export function CandleStickChart({ style, loading }: CandleStickChartProps): JSX.Element {
+  const chartRef = useRef<HTMLDivElement>(null);
+  return (
+    <Skeleton visible={loading ?? false}>
+      <div ref={chartRef} style={{ height: "100%", width: "100%", ...style }} />
+    </Skeleton>
+  );
+}
