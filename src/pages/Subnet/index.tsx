1e6b1553508564368ac9f173e3e163cf19e90466
maneetgoyal <mgoyal@manufacanalytics.com>
2024-01-09 10:21:25 +0530

    Refactor: add component

--- src/pages/Subnet/index.tsx
+++ src/pages/Subnet/index.tsx
@@ -1,7 +1,26 @@
+import { Card, useMantineColorScheme } from "@mantine/core";
 import { useParams } from "react-router-dom";
+import { Heatmap } from "../../charts/Heatmap";
 
 export function Subnet(): JSX.Element {
   const { subnet } = useParams<{ subnet: string }>(); // Ref: https://reactrouter.com/en/main/hooks/use-params
-
-  return <>Subnet {subnet}</>;
+  const { colorScheme } = useMantineColorScheme();
+  return (
+    <Card>
+      <Heatmap
+        title={subnet}
+        style={{ height: "50vh" }}
+        theme={colorScheme === "auto" ? "dark" : colorScheme}
+        // TODO: Add data
+        data={[]}
+        xAxis="minerID"
+        yAxis="validatorID"
+        visualAxis="weight"
+        visualAxisLabel="Weight"
+        xAxisLabel="NetUID"
+        yAxisLabel="Validator"
+        // TODO: Add loading
+      />
+    </Card>
+  );
 }
