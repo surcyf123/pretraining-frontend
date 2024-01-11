879cfbe76120e9686f4ac6ccea668596a8010a95
maneetgoyal <mgoyal@manufacanalytics.com>
2024-01-11 11:53:48 +0530

    feat: add scripts to run cron job

---
+++ api/cron/main.py
@@ -0,0 +1,8 @@
+def start():
+    print("Starting cron job.")
+    # TODO: add code to execute command "crontab metagraph.cron" to schedule cron job.
+
+
+def stop():
+    print("Stopping cron job.")
+    # TODO: add code to remove cron job.
