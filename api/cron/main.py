9e4fb2de746d4af29bb8841274603f2b1be47e82
maneetgoyal <mgoyal@manufacanalytics.com>
2024-01-11 12:58:50 +0530

    feat: start and stop script

--- api/cron/main.py
+++ api/cron/main.py
@@ -1,8 +1,18 @@
+from  subprocess import Popen
+from os import getcwd, path
+
 def start():
     print("Starting cron job.")
-    # TODO: add code to execute command "crontab metagraph.cron" to schedule cron job.
-
+    cronjobFilepath=path.join(getcwd(),"cron","metagraph.cron")
+    try:
+        proccess = Popen(["crontab",cronjobFilepath])
+        proccess.wait()
+        print("Cron job Started.")
+    except:
+        print("Cronfile is in invalid format.")
 
 def stop():
     print("Stopping cron job.")
-    # TODO: add code to remove cron job.
+    proccess = Popen(["crontab","-r"])
+    proccess.wait()
+    print("Cron job stopped.")
