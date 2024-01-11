5213ac490710c62e8846b4fe33fc3f223f25a431
maneetgoyal <mgoyal@manufacanalytics.com>
2024-01-11 13:09:40 +0530

    style: pretty

--- api/cron/main.py
+++ api/cron/main.py
@@ -1,18 +1,20 @@
-from  subprocess import Popen
+from subprocess import Popen
 from os import getcwd, path
+
 
 def start():
     print("Starting cron job.")
-    cronjobFilepath=path.join(getcwd(),"cron","metagraph.cron")
+    cronjobFilepath = path.join(getcwd(), "cron", "metagraph.cron")
     try:
-        proccess = Popen(["crontab",cronjobFilepath])
+        proccess = Popen(["crontab", cronjobFilepath])
         proccess.wait()
         print("Cron job Started.")
     except:
         print("Cronfile is in invalid format.")
 
+
 def stop():
     print("Stopping cron job.")
-    proccess = Popen(["crontab","-r"])
+    proccess = Popen(["crontab", "-r"])
     proccess.wait()
     print("Cron job stopped.")
