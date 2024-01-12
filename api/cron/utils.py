15ab2c0d589a011d12da2b3af49c66aff0892126
Maneet Goyal <mgoyal@manufacanalytics.com>
2024-01-12 11:40:21 +0530

    Update api/cron/utils.py

--- api/cron/utils.py
+++ api/cron/utils.py
@@ -3,6 +3,6 @@ from simplejson import dump
 
 
 def dumpData(filename: str, data: dict):
-    outputPath = path.join(getcwd(), "databank", filename)
+    outputPath = path.join(getcwd(), "data-bank", filename)
     with open(outputPath, "w") as f:
         dump(data, f, indent=2, ignore_nan=True)
