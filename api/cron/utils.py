966ba7fbb7dead9253d224ed6d0d28dfa55bb708
maneetgoyal <mgoyal@manufacanalytics.com>
2024-01-15 10:29:46 +0530

    feat: fetch validator data

--- api/cron/utils.py
+++ api/cron/utils.py
@@ -1,8 +1,31 @@
 from os import path, getcwd
 from simplejson import dump
+from pandas import Series
+from json import loads
 
 
 def dumpData(filename: str, data: dict):
     outputPath = path.join(getcwd(), "data-bank", filename)
     with open(outputPath, "w") as f:
         dump(data, f, indent=2, ignore_nan=True)
+
+
+def extractOriginalFormatData(runs):
+    validatorRunData = {}
+    for run in runs:
+        print(run.name)
+        runData = run.history()
+        if "original_format_json" in runData.columns:
+            originalFormatJsonData = runData["original_format_json"]
+            convertedData = []
+            if isinstance(originalFormatJsonData, Series):
+                targetList = originalFormatJsonData.to_list()
+                for ele in targetList:
+                    if isinstance(ele, str):
+                        convertedData.append(loads(ele))
+                    else:
+                        convertedData.append(ele)
+            else:
+                convertedData = originalFormatJsonData
+            validatorRunData[run.name] = convertedData
+    return validatorRunData
