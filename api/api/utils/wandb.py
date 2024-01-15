7cf58ae29f35eaed42eee66c0c82063fdc47bcba
maneetgoyal <mgoyal@manufacanalytics.com>
2024-01-15 13:36:15 +0530

    remove unused function

--- api/api/utils/wandb.py
+++ api/api/utils/wandb.py
@@ -1,11 +1,12 @@
 from pandas import DataFrame
 from json import load
 from datetime import datetime, timedelta
-from math import nan, isnan, isinf
+from math import nan
 from numpy import concatenate
 from functools import reduce
 from os import getcwd, path
 from fastapi import HTTPException
+from cron.utils import reducer, isValidUIDItem
 
 
 def smoothBestAverageLoss(data):
@@ -25,24 +26,6 @@ def smoothBestAverageLoss(data):
     return output
 
 
-def calculateBestAverageLoss(data: dict) -> dict:
-    output = data
-    for validatorID, validatorInfo in data.items():
-        for index, item in enumerate(validatorInfo):
-            uids = item.get("uids", [])
-            uidData = item.get("uid_data", {})
-            averageLosses = []
-            for uid in uids:
-                currentUIDData = uidData.get(str(uid), None)
-                if isValidUIDItem(currentUIDData) == True:
-                    averageLoss = currentUIDData.get("average_loss", None)
-                    averageLosses.append(averageLoss)
-            if len(averageLosses) > 0:
-                bestAverageLoss = min(averageLosses)
-                output[validatorID][index]["best_average_loss"] = bestAverageLoss
-    return output
-
-
 def transformValidatorRuns(runs: dict):
     output = []
     for validatorID, validatorInfo in runs.items():
@@ -56,29 +39,6 @@ def transformValidatorRuns(runs: dict):
                 }
             )
     return output
-
-
-def isValidUIDItem(item) -> bool:
-    output = True
-    if item["block"] is None or isnan(item["block"]) or isinf(item["block"]):
-        output = False
-    elif (
-        item["average_loss"] is None
-        or isnan(item["average_loss"])
-        or isinf(item["average_loss"])
-    ):
-        output = False
-    return output
-
-
-def reducer(value: dict) -> dict:
-    acc, curr, key = value["acc"], value["curr"], value["key"]
-    item = curr[key]
-    if item in acc:
-        acc[item].append(curr)
-    else:
-        acc[item] = [curr]
-    return acc
 
 
 def extractUIDs(runData: dict):
@@ -100,35 +60,6 @@ def extractUIDs(runData: dict):
     return output
 
 
-def parseRunID(runID: str) -> dict:
-    segments = runID.split("_")  # ["validator-id-year-month-date","hours-minutes-sec"]
-    id, year, month, date = segments[0].split("-")[1:]
-    timestamp = f"{date}-{month}-{year}-{segments[1]}"
-    parsedTimestamp = datetime.strptime(timestamp, "%d-%m-%Y-%H-%M-%S").timestamp()
-    return {"timestamp": parsedTimestamp, "validatorID": id}
-
-
-def createRunID(value: dict) -> str:
-    validatorID = f"validator-{value['validatorID']}"
-    formattedTimestamp = datetime.fromtimestamp(value["timestamp"]).strftime(
-        "%Y-%m-%d_%H-%M-%S"
-    )
-    return f"{validatorID}-{formattedTimestamp}"
-
-
-def filterLatestRuns(runs: dict) -> dict:
-    parsedRunIDs = [parseRunID(key) for key in runs.keys()]
-    sortedResults = sorted(parsedRunIDs, key=lambda x: x["timestamp"], reverse=True)
-    groups = reduce(
-        lambda acc, curr: reducer({"acc": acc, "curr": curr, "key": "validatorID"}),
-        sortedResults,
-        {},
-    )
-    filteredRunIDs = [createRunID(value[0]) for value in groups.values()]
-    filteredRuns = {key: runs[key] for key in filteredRunIDs}
-    return filteredRuns
-
-
 def filterValidatorRuns(data: dict, days: int) -> dict:
     output = {}
     threshold = (datetime.now() - timedelta(days=days)).timestamp()
@@ -145,9 +76,7 @@ def loadValidatorRuns(days: int) -> dict:
         with open(validatorRunsFilepath, "r") as file:
             validatorRuns = load(file)
             filteredRuns = filterValidatorRuns(validatorRuns, days)
-            recentRuns = filterLatestRuns(filteredRuns)
-            updatedRuns = calculateBestAverageLoss(recentRuns)
-        return updatedRuns
+        return filteredRuns
     except FileNotFoundError:
         # Ref: https://fastapi.tiangolo.com/tutorial/handling-errors/?h=error#raise-an-httpexception-in-your-code
         raise HTTPException(status_code=404, detail="Validator runs not found.")
