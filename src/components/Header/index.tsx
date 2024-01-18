17b90935fcec51cbfaec48b0e80bd855a6c5d3db
maneetgoyal <mgoyal@manufacanalytics.com>
2024-01-18 11:27:41 +0530

    refactor: minor

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,7 +9,9 @@ import {
   Text,
   ScrollArea,
   Burger,
+  useMantineTheme,
 } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import { IconChevronDown, IconLogout, IconMoonStars, IconSun } from "@tabler/icons-react";
 import { NavLink, useLocation } from "react-router-dom";
 import Logo from "./logo.png";
@@ -32,53 +34,59 @@ export function Header({
   const location = useLocation();
   const currentPathName = location.pathname;
   const { authStatus, signOut } = useAuthenticator((context) => [context.authStatus]);
-
+  const { breakpoints } = useMantineTheme();
+  const isMobile = useMediaQuery(`(max-width:${breakpoints.sm})`);
   return (
     <AppShell.Header p="xs">
       <Group justify="space-between" align="center">
         <Image {...Logo} h={30} alt="Openpretrain logo" />
-        <Group wrap="nowrap">
-          {NavLinks?.map(({ label, link, links }) =>
-            Array.isArray(links) ? (
-              <Menu key={link} trigger="hover">
-                <Menu.Target>
-                  <MantineNavLink
-                    component={NavLink}
-                    active={links.findIndex((linkItem) => linkItem.link === currentPathName) !== -1}
-                    to={link}
-                    label={<Text fw={500}>{label}</Text>}
-                    noWrap
-                    rightSection={<IconChevronDown size="1rem" />}
-                  />
-                </Menu.Target>
-                <Menu.Dropdown>
-                  <ScrollArea h="50vh">
-                    {links.map((item) => (
-                      <Menu.Item key={item.link}>
-                        <MantineNavLink
-                          component={NavLink}
-                          active={currentPathName === item.link}
-                          to={item.link}
-                          label={<Text>{item.label}</Text>}
-                        />
-                      </Menu.Item>
-                    ))}
-                  </ScrollArea>
-                </Menu.Dropdown>
-              </Menu>
-            ) : (
-              <MantineNavLink
-                key={link}
-                component={NavLink}
-                active={currentPathName === link}
-                to={link}
-                label={<Text fw={500}>{label}</Text>}
-                noWrap
-                title={`Go to ${label}`}
-              />
-            ),
-          )}
-        </Group>
+        {/* TODO: add nav bar */}
+        {isMobile === true ? null : (
+          <Group wrap="nowrap">
+            {NavLinks?.map(({ label, link, links }) =>
+              Array.isArray(links) ? (
+                <Menu key={link} trigger="hover">
+                  <Menu.Target>
+                    <MantineNavLink
+                      component={NavLink}
+                      active={
+                        links.findIndex((linkItem) => linkItem.link === currentPathName) !== -1
+                      }
+                      to={link}
+                      label={<Text fw={500}>{label}</Text>}
+                      noWrap
+                      rightSection={<IconChevronDown size="1rem" />}
+                    />
+                  </Menu.Target>
+                  <Menu.Dropdown>
+                    <ScrollArea h="50vh">
+                      {links.map((item) => (
+                        <Menu.Item key={item.link}>
+                          <MantineNavLink
+                            component={NavLink}
+                            active={currentPathName === item.link}
+                            to={item.link}
+                            label={<Text>{item.label}</Text>}
+                          />
+                        </Menu.Item>
+                      ))}
+                    </ScrollArea>
+                  </Menu.Dropdown>
+                </Menu>
+              ) : (
+                <MantineNavLink
+                  key={link}
+                  component={NavLink}
+                  active={currentPathName === link}
+                  to={link}
+                  label={<Text fw={500}>{label}</Text>}
+                  noWrap
+                  title={`Go to ${label}`}
+                />
+              ),
+            )}
+          </Group>
+        )}
         <Group>
           <ActionIcon onClick={onToggleColorScheme} variant="default">
             {colorScheme === "dark" ? <IconSun /> : <IconMoonStars />}
