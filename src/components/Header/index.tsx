1e58b6d6962307c24d725e948ddfaaefbaf5cfe3
maneetgoyal <mgoyal@manufacanalytics.com>
2024-01-18 13:05:34 +0530

    revert navbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -39,45 +39,7 @@ export function Header({
     <AppShell.Header p="xs">
       <Group justify="space-between" align="center">
         <Image {...Logo} h={30} alt="Openpretrain logo" />
-        {isMobile === true ? (
-          // Ref: https://mantine.dev/app-shell/?e=BasicAppShell&s=code
-          <AppShell.Navbar p="md">
-            {NavLinks?.map(({ label, link, links }) =>
-              Array.isArray(links) ? (
-                // Ref: https://mantine.dev/core/nav-link/#nested-navlinks
-                <MantineNavLink
-                  label={<Text fw={500}>{label}</Text>}
-                  noWrap
-                  rightSection={<IconChevronDown size="1rem" />}
-                >
-                  <ScrollArea h="50vh">
-                    {links.map((item) => (
-                      <MantineNavLink
-                        key={item.link}
-                        component={NavLink}
-                        active={currentPathName === item.link}
-                        to={item.link}
-                        label={<Text>{item.label}</Text>}
-                        onClick={onToggleNavbar}
-                      />
-                    ))}
-                  </ScrollArea>
-                </MantineNavLink>
-              ) : (
-                <MantineNavLink
-                  key={link}
-                  component={NavLink}
-                  active={currentPathName === link}
-                  to={link}
-                  label={<Text fw={500}>{label}</Text>}
-                  noWrap
-                  title={`Go to ${label}`}
-                  onClick={onToggleNavbar}
-                />
-              ),
-            )}
-          </AppShell.Navbar>
-        ) : (
+        {isMobile === true ? null : (
           <Group wrap="nowrap">
             {NavLinks?.map(({ label, link, links }) =>
               Array.isArray(links) ? (
